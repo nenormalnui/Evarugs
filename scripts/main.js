28e1efc1254daaa15abd3348d529b06f6c25d778
nenormalnui <72319525+nenormalnui@users.noreply.github.com>
2022-01-27 18:17:38 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+	jQuery("#user-city").text(ymaps.geolocation.city);
+	jQuery("#user-city2").text(ymaps.geolocation.city);
+}
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    speed: 400,
+    spaceBetween: 70,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    toggle: true,
+});
