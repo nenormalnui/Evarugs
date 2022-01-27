78287ae48f14a1b51ae5448fd3aa9f6b9e961c64
nenormalnui <72319525+nenormalnui@users.noreply.github.com>
2022-01-28 00:32:51 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,22 @@ const swiper = new Swiper('.swiper', {
     centeredSlides: true,
     slideToClickedSlide: true,
     toggle: true,
-});
+    keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+    breakpoints: {
+      320: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 1
+      },
+      1440: {
+        slidesPerView: 3
+      }
+    }
+  });
 
 window.onload = function () {
 	jQuery("#user-city").text(ymaps.geolocation.city);
